31043cf41df3ff4b275226d5374f56a1bc55bfa7
Rodolfo D. Arce S <rodolfoarce@technoma.com.py>
2019-08-13 15:39:40 -0400

    First commit with parameters and basic usage

---
+++ txt2sql.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+import sys, getopt
+
+def main (argv):
+	# Default values
+	host = 'localhost'
+	port = 5432
+	user = 'postgres'
+	password = ''
+	table = ''
+	column = ''
+	
+	#print('ARGV      :', argv)
+	
+	try:
+		opts, args = getopt.getopt(argv,'hH:p:u:P:t:c:', ["help", "host=", "port=", "user=", "password=", "table=", "column="])
+	except getopt.GetoptError:
+		print_help()
+		sys.exit(1)
+		
+	#print('OPTIONS   :', opts)
+	
+	# Quit when asking for help
+	for opt, arg in opts:
+		if opt in ('-h', '--help'):
+			print_help()
+			sys.exit(1)
+		elif opt in ("-t", "--table"):
+			table = arg
+		elif opt in ("-c", "--column"):
+			column = arg
+		elif opt in ("-H", "--host"):
+			host = arg
+		elif opt in ("-p", "--port"):
+			host = arg
+		elif opt in ("-u", "--user"):
+			user = arg
+		elif opt in ("-P", "--password"):
+			password = arg
+
+	# Check if column and table are set
+	if column == '' or table == '':
+		print_help()
+		sys.exit(1)
+		
+	print ("Tabla: ", table, " Columna: ", column)
+
+def print_help():
+	print("txt2sql.py -t <table> -c <column> [ -H <host> | -p <port> | -u <user> | -P <password> ]\nParameters -t (--table) and -c (--column) are REQUIRED")
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
